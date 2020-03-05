ba7b54b9163eb81e3f362549ef2276eecb09e34e
bonfacethaa <thaabonface@gmail.com>
2020-03-05 21:02:00 +0300

    refactor(common): add requirements file, ignore db and update BASE_IP

--- uploads/models.py
+++ uploads/models.py
@@ -1,19 +1,16 @@
 from django.core.files.storage import FileSystemStorage
 from django.db import models
 
-fs_one = FileSystemStorage(location='/home/adminsm/new1')
-fs_two = FileSystemStorage(location='/home/adminsm/new2')
-# fs_one = FileSystemStorage('/home/new')
-# fs_two = FileSystemStorage('/home/new2')
+BASE_PATH = '/home/adminsm/'
 
 
 class DocumentOne(models.Model):
     description = models.CharField(max_length=255, blank=True, null=True)
-    document = models.FileField(storage=FileSystemStorage(location='/home/adminsm/new1', base_url='/home/adminsm/new1/'))
+    document = models.FileField(storage=FileSystemStorage(location=BASE_PATH + '/new1', base_url=BASE_PATH + '/new1/'))
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
 
 class DocumentTwo(models.Model):
     description = models.CharField(max_length=255, blank=True, null=True)
-    document = models.FileField(storage=FileSystemStorage(location='/home/adminsm/new2', base_url='/home/adminsm/new2/'))
+    document = models.FileField(storage=FileSystemStorage(location=BASE_PATH + '/new2', base_url=BASE_PATH + '/new2/'))
     uploaded_at = models.DateTimeField(auto_now_add=True)
